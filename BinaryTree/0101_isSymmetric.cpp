47371b838f5bb1338ff4789e3fcf1a62f83aea0b
Pi <altmanpp@gmail.com>
2021-05-27 21:04:55 +0800

    Add files via upload

--- BinaryTree/0101_isSymmetric.cpp
+++ BinaryTree/0101_isSymmetric.cpp
@@ -24,10 +24,43 @@ public:
         else if (left != NULL && right == NULL) return false;
         else if (left == NULL && right == NULL) return true;
         else if (left->val != right->val) return false;
-        else return compare(left->left, right->right) && compare(left->right, right->left);
+        // 此时才做递归，下一层的判断
+        bool outside = compare(left->left, right->right);
+        bool inside = compare(left->right, right->left);
+        bool isSame = outside && inside;
+        return isSame;
     }
     bool isSymmetric(TreeNode* root) {
         if (root == NULL) return true;
         return compare(root->left, root->right);
+    }
+};
+
+
+// 迭代
+class Solution2 {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        queue<TreeNode*> que;
+        que.push(p);   
+        que.push(q);  
+        while (!que.empty()) {  
+            TreeNode* leftNode = que.front(); que.pop();
+            TreeNode* rightNode = que.front(); que.pop();
+            if (!leftNode && !rightNode) {  
+                continue;
+            }
+            if ((!leftNode || !rightNode || (leftNode->val != rightNode->val))) {
+                return false;
+            }
+            // 相对于求对称二叉树，这里两个树都要保持一样的遍历顺序
+            que.push(leftNode->left); 
+            que.push(rightNode->left); 
+            que.push(leftNode->right);  
+            que.push(rightNode->right);  
+        }
+        return true;
     }
 };
